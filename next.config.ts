cf28d87e4731894318bc33b0625c0f714262a0d5
jai <ghj21189590@gmail.com>
2025-09-17 15:06:20 +0800

    新增允許google圖片的請求

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: '*.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
